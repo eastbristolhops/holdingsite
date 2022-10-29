989a55a79d9436fa2d2c8a07528d81dee3d31450
jonathanw <jonwhewaycode@outlook.com>
2022-10-29 15:15:46 +0100

    sorted links

--- static/js/footerdate.js
+++ static/js/footerdate.js
@@ -1,6 +1,9 @@
 "use strict";
 
+let brewkettleyear = document.getElementById('brewkettleyear');
 let footerdate = document.getElementById("footeryear");
 let date = new Date().getFullYear();
 
+brewkettleyear.innerHTML = `${date}`
 footerdate.innerHTML = `2016-${date}`;
+
--- static/newsEvents/news_event_info.js
+++ static/newsEvents/news_event_info.js
@@ -1,15 +1,16 @@
 "use strict";
 
 let news =[
-    // "change this info and it will appear on the landing page under announcements",
+    "<b style='color: red'>*** BIG NEWS ***</b> We have changed our name, formerly East Bristol Hops, now <b>Bristol Hops Collective</b> we have \
+    essentially gone back to our roots of 2016 and are now using our original name, we feel we have outgrown East Bristol Hops as new member whould always\
+     ask if they could join but where unsure if it was possible as we \
+    where East Bristol Hops so now we feel we are more inclusing of the comunity we cover.",
 ]
 let events =[
-     "The Legandary Cropathon/Weigh-in @Dawkins Brewery",
-     "Brew day",
-     "Easton Promise launch Party @Dawkins Brewery, Lawnwood Rd",
+     '<b style="color: red">*** BIG NEWS ***</b> New Logo comming soon', 
+     'We now have a page dedicated to news go check out out'
 ]
 let events_dates_times = [
-     "Monday 12th September 5-8pm",
-     "Wednesday 14th September",
-     "Friday 21st October 5-9pm",
+     'early 2023', 
+     '<a href="pages/news.html">Click Here</a>'
 ]
